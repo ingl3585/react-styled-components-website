1a63f0f92315fc310b7c36cf4fc233b5b09467ff
Tony Ingle <ingleanthonyco@mgial.com>
2022-03-03 12:40:48 -0700

    setting up navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-function App() {
-	return <div className='App'></div>;
-}
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const App = () => {
+	return (
+		<Router>
+			<Navbar />
+		</Router>
+	);
+};
 
 export default App;
