3a1642272adfca3544d0509da97e0f340a228ea0
Tony Ingle <ingleanthonyco@gmail.com>
2022-03-15 11:38:25 -0600

    adding svgs

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,1 +1,17 @@
-f
+export const homeObjOne = {
+	id: 'about',
+	lightBg: false,
+	lightText: true,
+	lightTextDesc: true,
+	topLine: 'Premium Bank',
+	headline: 'Unlimited Transactions with Zero Fees',
+	description:
+		'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+	buttonLabel: 'Get started',
+	imgStart: false,
+	img: require('../../images/svg-1.svg'),
+	alt: 'Car',
+	dark: true,
+	primary: true,
+	darkText: false,
+};
