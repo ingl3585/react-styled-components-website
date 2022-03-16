7f89d3457db6d442c23229f2deb6be23c2f4880a
Tony Ingle <ingleanthonyco@gmail.com>
2022-03-16 08:40:10 -0600

    starting on sign up

--- src/components/SignIn/SignInElements.js
+++ src/components/SignIn/SignInElements.js
@@ -29,6 +29,15 @@ export const FormWrap = styled.div`
 `;
 
 export const Icon = styled(Link)`
-    margin-left: 32px;
-    margin-top: 32px;
-`
+	margin-left: 32px;
+	margin-top: 32px;
+	text-decoration: none;
+	color: #fff;
+	font-weight: 700;
+	font-size: 32px;
+
+	@media screen and (max-width: 480px) {
+		margin-left: 16px;
+		margin-top: 8px;
+	}
+`;
