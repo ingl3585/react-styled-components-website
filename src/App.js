3bb95db9cdd8f9ed846681b9d6239484dc75f3ba
Tony Ingle <ingleanthonyco@gmail.com>
2022-03-23 11:17:09 -0500

    deployed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages';
 import SignInPage from './pages/signin';
 
 const App = () => {
 	return (
-		<Router>
+		<Router basename='/'>
 			<Routes>
 				<Route exact path='/' element={<Home />} />
-				<Route exact path='/sign-in' element={<SignInPage />} />
+				<Route path='/sign-in' element={<SignInPage />} />
 			</Routes>
 		</Router>
 	);
